5e5302a7799ff21b80e26a21705ae7b7f8052209
Rich Young <git@richjyoung.co.uk>
2013-04-13 13:28:38 +0200

    Commenting LED.ino

--- CubeSat_Comms/LED.ino
+++ CubeSat_Comms/LED.ino
@@ -1,16 +1,20 @@
+// Initialise LED pin mode
 void LED_init() {
   pinMode(LED, OUTPUT);
   digitalWrite(LED,LOW);
 }
 
+// LED on
 void LED_on() {
   digitalWrite(LED,HIGH);
 }
 
+// LED off
 void LED_off() {
   digitalWrite(LED,LOW);
 }
 
+// Flash LED at specific interval indefinitely
 void LED_flash(int time) {
   LED_on();
   delay(time);
@@ -18,6 +22,7 @@ void LED_flash(int time) {
   delay(time);
 }
 
+// Error indicator
 void Error() {
   while(true)
     LED_flash(100);
