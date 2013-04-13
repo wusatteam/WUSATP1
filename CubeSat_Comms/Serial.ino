9ed0a802f36c3a44c01205bf3ba5ab9e41309e53
Rich Young <git@richjyoung.co.uk>
2013-04-13 14:01:10 +0200

    Commenting Serial.ino

--- CubeSat_Comms/Serial.ino
+++ CubeSat_Comms/Serial.ino
@@ -1,12 +1,15 @@
+// Initialise Serial Port
 void Serial_init() {
   Serial.begin(38400);
 
 #ifdef OPTION_SERIAL_WAIT
+  // Wait for Arduino Leonard USB-UART to connect
   while(!Serial){
   }
 #endif
 }
 
+// Process received command from CMD_PORT
 void Serial_command(){
   char cmd[3];
   int clen = CMD_PORT.readBytes(cmd,3);
@@ -17,7 +20,7 @@ void Serial_command(){
     if(cmd[1]=='D') Cmd_shutdown(); //SDN
     if(cmd[1]=='E') Cmd_set(); //SET
     break;
-  case 'P':
+  case 'P': //PAU
     Cmd_pause();
     break;
   case 'R':
@@ -33,7 +36,7 @@ void Serial_command(){
     CMD_PORT.read();
 }
 
-
+// Print string with timestamp
 void DEBUGLN(char* str){
 #ifdef DEBUG_ON
   Serial.print(millis(),DEC);
@@ -42,11 +45,13 @@ void DEBUGLN(char* str){
 #endif
 }
 
+// Shutdown - currently just stops execution
 void Cmd_shutdown(){
   CMD_PORT.println(F("SDN"));
   Error();
 }
 
+// Set date and time, e.g. SETMar 06 201312:34:00
 void Cmd_set(){
   char time[8];
   char date[11];
@@ -56,19 +61,22 @@ void Cmd_set(){
   CMD_PORT.println(F("D/T Updated."));
 }
 
+// Clear log files
 void Cmd_reset(){
   clearLog=true;
 }
 
+// Pause execution
 void Cmd_pause(){
   CMD_PORT.println("PAUSE");
   char cmd[3];
+  // Wait until 3 characters are received
   while(CMD_PORT.readBytes(cmd,3)<3)
     ;
 }
 
+// Print Header and build information
 void Serial_header(){
-  // Header and build information
   Serial.println(F("CubeSat FW - Comms"));
   Serial.print(F("V "));
   Serial.print(__DATE__);
